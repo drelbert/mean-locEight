010d7c84a548547d677826ca84e45ccbacc46e32
drelbert <danrelbert@gmail.com>
2019-07-08 16:49:15 -0400

    Optimizations

--- client/backend/routes/people.js
+++ client/backend/routes/people.js
@@ -1,87 +1,35 @@
 const express = require("express");
 
-const People = require('../models/people');
-const checkAuth = require('../middleware/check-auth');
+const PeopleController = require ("../controllers/people");
+
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
-//adding simple change
 
-router.post("",
-checkAuth,
- (req, res, next) => {
-  const people = new People({
-    //Passing in the JavaScript object from models/people
-    name: req.body.name,
-    role: req.body.role
-  });
-  people
-  .save()
-  .then(addedPerson => {
-    res.status(201).json({
-      message: 'Person Added',
-      personId: addedPerson._id
-   });
-  });
-});
-
-
-router.put("/:id",
+router.post(
+  "",
   checkAuth,
-  (req, res, next) => {
-  const person = new People({
-    _id: req.body.id,
-    name: req.body.name,
-    role: req.body.role
-  })
-  People.updateOne({_id: req.params.id}, person).then(result => {
-    res.status(200).json({ message: "Person Updated!" });
-  });
-});
+  PeopleController.addPerson
+ );
 
 
-router.get("", (req, res, next) => {
-  const pageSize = +req.query.pagesize;
-  const currentPage = +req.query.page;
-  const peopleQuery = People.find();
-  let fetchedPeople;
-  if (pageSize && currentPage) {
-    peopleQuery
-      .skip(pageSize * (currentPage -1))
-      .limit(pageSize);
-  }
-   peopleQuery
-   .then(documents => {
-     fetchedPeople = documents;
-    return People.count();
-   })
-    .then(count => {
-      res.status(200).json({
-        message: 'People fetched successfully',
-        people: fetchedPeople,
-        maxPeople: count
-    });
-  });
-});
-
-
-router.get("/:id", (req, res, next) => {
-  People.findById(req.params.id).then(person => {
-    if(person) {
-      res.status(200).json(person);
-    } else {
-      res.status(404).json({message: "Person Not Found"});
-    }
-  });
-});
-
-router.delete("/:id",
+router.put(
+  "/:id",
   checkAuth,
-  (req, res, next) => {
-  People.deleteOne({_id: req.params.id}).then (result => {
-    console.log(result);
-    res.status(200).json({message: 'Person removed'});
-  });
-});
+  PeopleController.updatePerson
+  );
+
+
+router.get("", PeopleController.getPeople);
+
+
+router.get("/:id", PeopleController.getPersonById);
+
+router.delete(
+  "/:id",
+  checkAuth,
+  PeopleController.deletePerson
+  );
 
 module.exports = router;
